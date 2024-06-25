13b491d08e9e0de872db25cf7d7ecf13232e54ab
khamaruayan <169046571+khamaruayan@users.noreply.github.com>
2024-06-25 20:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//Javascript Errors and Error Handling
+"use strict";
+const MakeError=()=>{
+    let i=1;
+    while(i<=5){
+        try{
+            if(i%2!==0){
+                throw new Error("Odd number!");
+            }
+            console.log("Even number!");
+     
+         }catch(err){
+             console.error(err.stack);
+         } finally{
+             console.log("........finally");
+             i++;
+         }
+
+    }
+    
+};
+MakeError();
+
